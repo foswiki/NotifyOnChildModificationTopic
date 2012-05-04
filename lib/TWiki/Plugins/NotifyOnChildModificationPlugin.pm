8018ae03b6be19c2007043fdf2724251c47eb21a
OlivierRaginel <OlivierRaginel@0b4bb1d4-4e5a-0410-9cc4-b2b747904278>
2012-05-04 13:27:49 +0000

    Item11808: perltidy them all, so people are not accidentally impacted by the new enforced rule -- Sorry Micha, could not find a way to define specific values within the file
    git-svn-id: http://svn.foswiki.org/trunk/NotifyOnChildModificationTopic@14686 0b4bb1d4-4e5a-0410-9cc4-b2b747904278

--- lib/TWiki/Plugins/NotifyOnChildModificationPlugin.pm
+++ lib/TWiki/Plugins/NotifyOnChildModificationPlugin.pm
@@ -3,12 +3,12 @@
 # Copyright (C) 2000-2003 Andrea Sterbini, a.sterbini@flashnet.it
 # Copyright (C) 2001-2003 Peter Thoeny, peter@thoeny.com
 #
-# initPlugin is required, all other are optional. 
+# initPlugin is required, all other are optional.
 # For increased performance, all handlers except initPlugin are
 # disabled. To enable a handler remove the leading DISABLE_ from
 # the function name. Remove disabled handlers you do not need.
 #
-# NOTE: To interact with TWiki use the official TWiki functions 
+# NOTE: To interact with TWiki use the official TWiki functions
 # in the TWiki::Func module. Do not reference any functions or
 # variables elsewhere in TWiki!!
 
@@ -17,9 +17,9 @@ package TWiki::Plugins::NotifyOnChildModificationPlugin;
 
 # =========================
 use vars qw(
-        $web $topic $user $installWeb $VERSION $RELEASE $pluginName
-        $debug $userListSuffix $emailTextSuffix
-    );
+  $web $topic $user $installWeb $VERSION $RELEASE $pluginName
+  $debug $userListSuffix $emailTextSuffix
+);
 
 # This should always be $Rev$ so that TWiki can determine the checked-in
 # status of the plugin. It is used by the build automation tools, so
@@ -31,107 +31,112 @@ $VERSION = '$Rev$';
 # of the version number in PLUGINDESCRIPTIONS.
 $RELEASE = 'Dakar';
 
-
 $userListSuffix = 'NotifyUsers';
 $mailTextSuffix = 'NotifyText';
 
-$pluginName = 'NotifyOnChildModificationPlugin';  # Name of this Plugin
+$pluginName = 'NotifyOnChildModificationPlugin';    # Name of this Plugin
 
 # =========================
-sub initPlugin
-{
+sub initPlugin {
     ( $topic, $web, $user, $installWeb ) = @_;
 
     # check for Plugins.pm versions
-    if( $TWiki::Plugins::VERSION < 1 ) {
+    if ( $TWiki::Plugins::VERSION < 1 ) {
         TWiki::Func::writeWarning(
-	  "Version mismatch between $pluginName and Plugins.pm" );
+            "Version mismatch between $pluginName and Plugins.pm");
         return 0;
     }
 
     # Get plugin debug flag
-    $debug = TWiki::Func::getPreferencesFlag( "\U$pluginName\E_DEBUG" );
-$debug=1;
+    $debug = TWiki::Func::getPreferencesFlag("\U$pluginName\E_DEBUG");
+    $debug = 1;
 
     # Plugin correctly initialized
     TWiki::Func::writeDebug(
-      "- TWiki::Plugins::${pluginName}::initPlugin( $web.$topic ) is OK" )
+        "- TWiki::Plugins::${pluginName}::initPlugin( $web.$topic ) is OK")
       if $debug;
     return 1;
 }
 
 # =========================
-sub beforeSaveHandler
-{
+sub beforeSaveHandler {
 ### my ( $text, $topic, $web ) = @_;   # do not uncomment, use $_[0], $_[1]... instead
 
-    &TWiki::Func::writeDebug(
-      "${pluginName}::beforeSaveHandler( $_[2].$_[1] )" ) if $debug;
+    &TWiki::Func::writeDebug("${pluginName}::beforeSaveHandler( $_[2].$_[1] )")
+      if $debug;
 
     # This handler is called by TWiki::Store::saveTopic just before
     # the save action.
     my $text = "";
     my $meta = "";
     ( $meta, $text ) = &TWiki::Func::readTopic( $_[2], $_[1] );
-    my %parent = $meta->findOne( "TOPICPARENT" );
+    my %parent = $meta->findOne("TOPICPARENT");
 
     # Find topics with the list of users to notify and the
     # text to send by email
-    if( $parent{"name"} =~ /([^.]*)[.]([^.]*)/ ){ $pname = $2; $pweb = $1; }
-    else{ $pname = $parent{"name"}; $pweb = $_[2]; }
-    my $userTopic="$pname$userListSuffix";
-    my $mailTopic="$pname$mailTextSuffix";
-    my $existsUsers=&TWiki::Func::topicExists($pweb,$userTopic);
-    my $existsText=&TWiki::Func::topicExists($pweb,$mailTopic);
-    if( $existsUsers && ! $existsText ){
-      $mailTopic=$mailTextSuffix;
-      $existsText=&TWiki::Func::topicExists($pweb,$mailTopic);
-      }
+    if ( $parent{"name"} =~ /([^.]*)[.]([^.]*)/ ) { $pname = $2; $pweb = $1; }
+    else { $pname = $parent{"name"}; $pweb = $_[2]; }
+    my $userTopic   = "$pname$userListSuffix";
+    my $mailTopic   = "$pname$mailTextSuffix";
+    my $existsUsers = &TWiki::Func::topicExists( $pweb, $userTopic );
+    my $existsText  = &TWiki::Func::topicExists( $pweb, $mailTopic );
+    if ( $existsUsers && !$existsText ) {
+        $mailTopic = $mailTextSuffix;
+        $existsText = &TWiki::Func::topicExists( $pweb, $mailTopic );
+    }
 
     # Send email notification to users
-    if( $existsUsers && $existsText ){
-      my @notifylist = TWiki::getEmailNotifyList( $pweb, $userTopic );
-      my $notifylist = join ', ', @notifylist;
-      if( $#notifylist >= 0 ){
-        ( $meta, $text ) = &TWiki::Func::readTopic( $pweb, $mailTopic );
-        my %mf=$meta->findOne("FIELD","NotifyOnChildModificationFormFrom");
-        my %ms=$meta->findOne("FIELD","NotifyOnChildModificationFormSubject");
-        my %mh=$meta->findOne("FIELD","NotifyOnChildModificationFormHeaders");
-        my $from=$mf{"value"},$subj=$ms{"value"},$head=$mh{"value"};
-        if($from eq ""){ $from="%WIKIWEBMASTER%"; }
-	if($subj eq ""){ $subj="Modification on $_[1] ($pname child)"; }
-	my $mail = "From: $from\nTo: $notifylist\nSubject: $subj";
-        if(!$head eq ""){ $mail .= "\n$head"; }
-        $text = "$mail\n\n$text";
-	$text =~ s/%TOPICPARENT%/$pname/g;
-        $theText = &TWiki::Func::expandCommonVariables($text, $_[1] );
-	&TWiki::Func::writeDebug(
-          "${pluginName}::beforeSaveHandler - Sending mail notification to:".
-          " $notifylist" ) if $debug;
-        my $error = &TWiki::Net::sendEmail( $text );
-	if( $error ) {
-          &TWiki::Func::writeDebug(
-            "${pluginName}::beforeSaveHandler - Cannot send email" .
-	    "(see email text below)\n$text" )
-            if $debug;
-          }
+    if ( $existsUsers && $existsText ) {
+        my @notifylist = TWiki::getEmailNotifyList( $pweb, $userTopic );
+        my $notifylist = join ', ', @notifylist;
+        if ( $#notifylist >= 0 ) {
+            ( $meta, $text ) = &TWiki::Func::readTopic( $pweb, $mailTopic );
+            my %mf =
+              $meta->findOne( "FIELD", "NotifyOnChildModificationFormFrom" );
+            my %ms =
+              $meta->findOne( "FIELD", "NotifyOnChildModificationFormSubject" );
+            my %mh =
+              $meta->findOne( "FIELD", "NotifyOnChildModificationFormHeaders" );
+            my $from = $mf{"value"}, $subj = $ms{"value"}, $head = $mh{"value"};
+            if ( $from eq "" ) { $from = "%WIKIWEBMASTER%"; }
+            if ( $subj eq "" ) {
+                $subj = "Modification on $_[1] ($pname child)";
+            }
+            my $mail = "From: $from\nTo: $notifylist\nSubject: $subj";
+            if ( !$head eq "" ) { $mail .= "\n$head"; }
+            $text = "$mail\n\n$text";
+            $text =~ s/%TOPICPARENT%/$pname/g;
+            $theText = &TWiki::Func::expandCommonVariables( $text, $_[1] );
+            &TWiki::Func::writeDebug(
+"${pluginName}::beforeSaveHandler - Sending mail notification to:"
+                  . " $notifylist" )
+              if $debug;
+            my $error = &TWiki::Net::sendEmail($text);
+
+            if ($error) {
+                &TWiki::Func::writeDebug(
+                        "${pluginName}::beforeSaveHandler - Cannot send email"
+                      . "(see email text below)\n$text" )
+                  if $debug;
+            }
+            else {
+                &TWiki::Func::writeDebug(
+                    "${pluginName}::beforeSaveHandler - Mail notification sent")
+                  if $debug;
+            }
+        }
         else {
-          &TWiki::Func::writeDebug(
-            "${pluginName}::beforeSaveHandler - Mail notification sent" )
-            if $debug;
-          }
+            &TWiki::Func::writeDebug(
+                "${pluginName}::beforeSaveHandler - user list to notify is void"
+            ) if $debug;
         }
-      else {
-        &TWiki::Func::writeDebug(
-          "${pluginName}::beforeSaveHandler - user list to notify is void")
-          if $debug;
-        }
-      }
+    }
     else {
-      &TWiki::Func::writeDebug(
-        "${pluginName}::beforeSaveHandler - Cannot find topic $userTopic" .
-        " or $mailTopic" ) if $debug;
-      }
+        &TWiki::Func::writeDebug(
+            "${pluginName}::beforeSaveHandler - Cannot find topic $userTopic"
+              . " or $mailTopic" )
+          if $debug;
+    }
 }
 
 1;
